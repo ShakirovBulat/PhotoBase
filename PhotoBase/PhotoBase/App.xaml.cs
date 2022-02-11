7968dc2604aa8237ab5bb292c234e0438ca013a9
булат шакиров <a2009-1@mail.ru>
2022-02-11 15:32:13 +0300

    add photo image end

--- PhotoBase/PhotoBase/App.xaml.cs
+++ PhotoBase/PhotoBase/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using PhotoBase.DB;
+using System;
+using System.IO;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -6,11 +8,25 @@ namespace PhotoBase
 {
     public partial class App : Application
     {
+        public const string DB_NAME = "clientsProj.db";
+        public static CRUDOperation db;
+        public static CRUDOperation Db
+        {
+            get
+            {
+                if (db == null)
+                {
+                    db = new CRUDOperation(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), DB_NAME));
+                }
+                return db;
+            }
+        }
+
         public App()
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart()
---
+++ PhotoBase/PhotoBase/TakePhoto.xaml.cs
@@ -0,0 +1,26 @@
+﻿using PhotoBase.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PhotoBase
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TakePhoto : ContentPage
+    {
+        readonly ProjectPhoto im;
+        public TakePhoto(ProjectPhoto im)
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+            this.im = im;
+            name.Text = im.Name;
+            img.Source = im.Imagepath;
+        }
+    }
+}
