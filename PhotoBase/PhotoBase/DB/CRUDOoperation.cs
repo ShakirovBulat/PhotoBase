36770ebc0a9c1c7c2b4f22eb578823f8fe3a43e6
булат шакиров <a2009-1@mail.ru>
2022-02-11 15:30:00 +0300

    db add

---
+++ PhotoBase/PhotoBase/DB/CRUDOoperation.cs
@@ -0,0 +1,35 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using PhotoBase.Models;
+using System.Text;
+
+namespace PhotoBase.DB
+{
+    public class CRUDOperation
+    {
+        SQLiteConnection db;
+        public CRUDOperation(string databasePath)
+        {
+            db = new SQLiteConnection(databasePath);
+            db.CreateTable<ProjectPhoto>();
+        }
+        public IEnumerable<ProjectPhoto> GetProjects()
+        {
+            return db.Table<ProjectPhoto>();
+        }
+        public int SaveItem(ProjectPhoto projectphoto)
+        {
+            if (projectphoto.Id != 0)
+            {
+                db.Update(projectphoto);
+                return projectphoto.Id;
+            }
+            else
+            {
+                return db.Insert(projectphoto);
+            }
+        }
+
+    }
+}
---
+++ PhotoBase/PhotoBase/DB/ISqlite.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhotoBase.DB
+{
+    public interface ISqlite
+    {
+        string GetDatabasePath(string filename);
+    }
+}
